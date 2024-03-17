7e6361d3c57edf1ddbe98edcfc6b960e314a4ffd
DeaPinkMe <74881020+deapinkme@users.noreply.github.com>
2024-03-17 05:33:52 +0000

    current work

--- contracts/DemoGameABI.ts
+++
@@ -1,9 +0,0 @@
-export const abi = [
-  {
-    name: "saveAddress",
-    type: "function",
-    stateMutability: "nonpayable",
-    inputs: [{ internalType: "address", name: "_newAddress", type: "address" }],
-    outputs: [],
-  },
-] as const;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,33 @@ import { WagmiProvider } from "wagmi";
 import { lineaTestnet, sepolia } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
+// Define FHEnix chain configuration
+  const fhenixChain = {
+    id: "42069",
+    name: 'Fhenix Frontier',
+    network: 'fhenix',
+    nativeCurrency: { name: 'FHE Token', symbol: 'FHE', decimals: 18 },
+    rpcUrls: {
+      default: {
+        http: ["https://api.testnet.fhenix.zone:7747/"],
+      },
+      public: {
+        http: ["https://api.testnet.fhenix.zone:7747/"],
+      },
+    },
+    blockExplorers: {
+      default: {
+        name: 'Fhenix Explorer',
+        url: "https://explorer.testnet.fhenix.zone/",
+      },
+    },
+  }
+
+// Add FHEnix chain to the existing chains array
 const config = getDefaultConfig({
   appName: "Cloak Coin App",
   projectId: "YOUR_PROJECT_ID",
-  chains: [lineaTestnet, sepolia],
+  chains: [sepolia, fhenixChain], // Include fhenixChain here
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
